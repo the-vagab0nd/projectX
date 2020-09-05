21cd3e7e63af20e3240c6581f6fb028354ed58d5
the-vagab0nd <lit2018008.ac.in>
2020-09-06 00:06:30 +0530

    progress

--- Resources/models.py
+++ Resources/models.py
@@ -4,10 +4,15 @@ from College.models import Batch, Course
 
 
 class Lecture(models.Model):
+    number = models.IntegerField()
+    lecture_date = models.DateField()
     batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ('number',)
 
 
 class Video(models.Model):
     embeded_code = models.CharField(max_length=100)
-    lecture = models.CharField(max_length=100)
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+
